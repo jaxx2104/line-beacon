aecbe177bd25e57e1f59e6c91cf1c18804afbf3c
iwa <jaxx2104@gmail.com>
2016-10-10 14:57:14 +0900

    add composer

--- index.php
+++ index.php
@@ -1,10 +1,31 @@
 <?php
-$json_string = file_get_contents('php://input');
-$json_object = json_decode($json_string);
+require("vendor/autoload.php");
 
-foreach ($json_object->events as $event) {
+use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use \LINE\LINEBot;
+
+// 設定
+$token = file_get_contents("token.txt");
+
+// インスタンス
+$httpClient = new CurlHTTPClient($token);
+$bot = new LINEBot($httpClient, ['channelSecret' => $token]);
+
+// webhook
+$jsonStr = file_get_contents('php://input');
+$jsonObj = json_decode($jsonStr);
+
+// 不正なリクエスト
+if (empty($jsonObj)) {
+    var_dump("request to line-beacon");
+    return;
+}
+
+// リクエスト処理
+foreach ($jsonObj->events as $event) {
     if('message' == $event->type){
-        file_put_contents("message.json", json_encode($event));
+        // debug
+        //file_put_contents("message.json", json_encode($event));
         $text = $event->message->text;
 
         if (preg_match("/休み/", $text)) {
@@ -18,36 +39,13 @@ foreach ($json_object->events as $event) {
         if (preg_match("/帰/", $text)) {
             $text = "[退社]お疲れ様!!";
         }
+        $response = $bot->replyText($event->replyToken, $text);
 
-        api_post_request($event->replyToken, $text);
-    }else if('beacon' == $event->type){
+    } else if('beacon' == $event->type) {
+        // debug
         file_put_contents("beacon.json", json_encode($event));
-        api_post_request($event->replyToken, '[出社]おはよう(ビーコン接近)');
+        $text = '[出社]おはよう(ビーコン接近)';
+        $response = $bot->replyText($event->replyToken, $text);
+
     }
-}
-
-
-function api_post_request($token, $message) {
-    $url = 'https://api.line.me/v2/bot/message/reply';
-    $channel_access_token = 'hYdyTTEekncXevDUTHxckKjdwUNnPIejxpRdpmhLNZ5GuGpCh/APpvFUi3816bHp8c9e7+qd62JzZb+1OZYjucMwKBLqcCmMD6EsL5ST5MwXB/OULBIser2Hy6OUH5x+Q3YobyyjY1I0i2WaHP8ffQdB04t89/1O/w1cDnyilFU=';
-    $headers = array(
-        'Content-Type: application/json',
-        "Authorization: Bearer {$channel_access_token}"
-    );
-    $post = array(
-        'replyToken' => $token,
-        'messages' => array(
-            array(
-                'type' => 'text',
-                'text' => $message
-            )
-        )
-    );
-
-    $curl = curl_init($url);
-    curl_setopt($curl, CURLOPT_POST, true);
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($post));
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_exec($curl);
 }
